1664c9e72c3bb4548832ff2ed2fb997577a451de
eryandrade1995 <erywelton.2014@gmail.com>
2022-01-28 11:45:18 -0300

    Introdução ao consumo da PokeAPI com utilização de hooks e etyled components

---
+++ example05/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import GlobalStyle from "./styles/global";
+
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Pokemon from "./pages/Pokemon/Pokemon";
+import Details from "./pages/Details/Details";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/pokemon" element={<Pokemon />} />
+        <Route path="/pokemon/details" element={<Details />} />
+      </Routes>
+      <GlobalStyle />
+    </BrowserRouter>
+  );
+}
+
+export default App;
